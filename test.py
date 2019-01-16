3c582ecfbc8c8ccc8787b20986a25587ddb92642
CCNUHemingZhang <CCNUHemingZhang@gmail.com>
2019-01-17 01:16:44 +0800

    add test connection code

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
-print ("Hello, world!")
+import pymysql 
+
+# Open database connection
+db = pymysql.connect(user = "root", password = "root",
+                     host = "127.0.0.1",
+                     database = "quora" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Drop table if it already exist using execute() method.
+cursor.execute("DROP TABLE IF EXISTS MOVIES")
+
+# Create table as per requirement
+sql = """CREATE TABLE MOVIES (
+   Title  CHAR(20) NOT NULL,
+   Text CHAR(20) )"""
+
+cursor.execute(sql)
+
+# disconnect from server
+db.close()
