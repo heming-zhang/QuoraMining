ae459d6c4a3b9b7550a58be4666ccd803aa7d867
CCNUHemingZhang <CCNUHemingZhang@gmail.com>
2019-04-10 22:13:34 +0800

    new

--- main_mining.py
+++ main_mining.py
@@ -3,34 +3,48 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
-from gensim import corpora
+from gensim import corpora, models
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+import spacy
+import datetime
 import gensim
 import nltk
-# nltk.download()
+import collections
 import re
 import string
 import numpy as np
 
 
 def classify_doc():
-    alltextlist_feb = []
+    alltextlist = []
     tv_sitcoms_answertext = DataBase(0, '0', '0', 0, '0', '0', 0).select_answertext()
     for text in tv_sitcoms_answertext: 
         answercount = int(text[1])
         date = str(text[2])
         question = str(text[4])
-        # view_weight = int(text[5]) # weight to improve topic mining
+        view_weight = (int(text[5]) // 50) + 1 # weight to improve topic mining
         answer = str(text[6])
-        # 剩下的事情就是将文本写入文件，然后把数进行清洗，然后进行主题挖掘
-        if date.find('2019/1')>=0:
-            # alltextlist_feb.append(question) # use append or just use '+' to aggregate string
-            if answercount > 0 : alltextlist_feb.append(answer)
+        datenorm = datetime.datetime.strptime(date, '%Y/%m/%d')
+        datenum = int(datenorm.strftime('%Y%m%d'))
+        # change datenum limitation to control weeks
+        if datenum >= 20190321:
+            alltextlist.append(question) # use append or just use '+' to aggregate string
+            if answercount > 0 : 
+                for i in range(view_weight): # use view_weight to add weight
+                    alltextlist.append(answer)
             # print(date, question, view_weight, answer)
-    # print(alltextlist_feb)
-    return alltextlist_feb
+    # print(alltextlist)
+    return alltextlist
 
 def text_clean_set():
-    stop = set(stopwords.words('english'))
+    stop = stopwords.words('english')
+    stop.extend(['movies', 'movie' 
+        'shows', 'show',
+        'films', 'film',
+        'tv', 'television'])
     exclude = set(string.punctuation)
     lemma = WordNetLemmatizer()
     return stop, exclude, lemma
@@ -39,42 +53,94 @@ def text_clean_set():
 def text_clean(text):
     stop, exclude, lemma= text_clean_set()
     num_free = re.sub(r'\d+', '', text)
-    punc_free = "".join(ch for ch in num_free if ch not in exclude)
-    stop_free = " ".join([i for i in punc_free.lower().split() if i not in stop])
-    punc_free = "".join(ch for ch in stop_free if ch not in exclude)
-    normalized = " ".join(lemma.lemmatize(word) for word in punc_free.split())
-
+    punc_free = re.sub("—", "", num_free)
+    punc_free0 = re.sub("\'", "", punc_free)
+    punc_free1 = "".join(ch for ch in punc_free0 if ch not in exclude)
+    stop_free1 = " ".join([i for i in punc_free1.lower().split() if i not in stop])
+    punc_free2 = "".join(ch for ch in stop_free1 if ch not in exclude)
+    normalized = " ".join(lemma.lemmatize(word) for word in punc_free2.split())
     return normalized
 
+
 def text_clean_run():
-    text_cleaned = []
-    alltextlist_feb = classify_doc()
-    text_cleaned0 = [text_clean(text).split() for text in alltextlist_feb] 
-    fw = open('./textfiles/Feb, 2019.txt', 'w')
-    for list1 in text_cleaned0:
+    text_cleaned0 = [] # text_cleaned0 has only one list
+    alltextlist = classify_doc()
+    text_cleaned2 = [text_clean(text).split() for text in alltextlist] 
+    fw = open('./textfiles/Mar4, 2019.txt', 'w')
+    for list1 in text_cleaned2:
         for list2 in list1:
-            text_cleaned.append(list2)
+            text_cleaned0.append(list2)
             if list2 == "": continue
             else:
                 list2 = list2 + "\t"
                 fw.write(list2)
 
-    # fr = open('./textfiles/Feb, 2019.txt', 'r')
+    # fr = open('./textfiles/Mar4, 2019.txt', 'r')
     # print(fr.read())
-    # print(text_cleaned0)
-    return text_cleaned0
+    # print(text_cleaned0) # text_cleaned2 has many lists in list
+    return text_cleaned2, text_cleaned0
+
 
 def lda_model():
-    text_cleaned0 = text_clean_run()
-    dictionary = corpora.Dictionary(text_cleaned0)
-    doc_term_matrix = [dictionary.doc2bow(doc) for doc in text_cleaned0]
-
+    text_cleaned2, text_cleaned0 = text_clean_run()
+    dictionary = corpora.Dictionary(text_cleaned2)
+    # dictionary.filter_extremes(no_below=15, no_above=0.5, keep_n=100000)
+    doc_term_matrix = [dictionary.doc2bow(doc) for doc in text_cleaned2]
+    # using Bag of Words
     Lda = gensim.models.ldamodel.LdaModel
-    ldamodel = Lda(doc_term_matrix, num_topics=10, id2word = dictionary, passes=50)
-    print(ldamodel.print_topics(num_topics=10, num_words=10))
+    ldamodel = Lda(doc_term_matrix, num_topics=5, id2word = dictionary, passes=50)
+    print(ldamodel.print_topics(num_topics=5, num_words=8))
+    # Compute Perplexity
+    print('\nPerplexity: ', ldamodel.log_perplexity(doc_term_matrix))  # a measure of how good the model is. lower the better.
+    # Compute Coherence Score
+    coherence_model_lda = CoherenceModel(model=ldamodel, texts=text_cleaned2, dictionary=dictionary, coherence='c_v')
+    coherence_lda = coherence_model_lda.get_coherence()
+    print('\nCoherence Score: ', coherence_lda)
 
 
+def tfidf_model():
+    text_cleaned2, text_cleaned0  = text_clean_run()
+    dictionary = corpora.Dictionary(text_cleaned2)
+    doc_term_matrix = [dictionary.doc2bow(doc) for doc in text_cleaned2]
+    # using TF-IDF
+    Lda = gensim.models.ldamodel.LdaModel
+    tfidf = models.TfidfModel(doc_term_matrix)
+    corpus_tfidf = tfidf[doc_term_matrix]
+    ldamodel_tfidf = Lda(corpus_tfidf, num_topics=10, id2word = dictionary, passes=50)
+    print(ldamodel_tfidf.print_topics(num_topics=10, num_words=5))
+
+    # Compute Perplexity
+    print('\nPerplexity: ', ldamodel_tfidf.log_perplexity(doc_term_matrix))  # a measure of how good the model is. lower the better.
+    # Compute Coherence Score
+    coherence_model_lda = CoherenceModel(model=ldamodel_tfidf, texts=text_cleaned2, dictionary=dictionary, coherence='c_v')
+    coherence_tfidf = coherence_model_lda.get_coherence()
+    print('\nCoherence Score: ', coherence_tfidf)
+
+
+def kmeans_model():
+    num_clusters = 5
+    km = KMeans(n_clusters = num_clusters)
+    text_cleaned2, text_cleaned0 = text_clean_run()
+    dictionary = corpora.Dictionary(text_cleaned2)
+    doc_term_matrix = [dictionary.doc2bow(doc) for doc in text_cleaned2]
+    # using TF-IDF and Clusterclear
+    tfidf = models.TfidfModel(doc_term_matrix)
+    corpus_tfidf = tfidf[doc_term_matrix]
+    km.fit(text_cleaned0)
+    clusters = km.labels_.tolist()
+    print(clusters)
+
+
+def get_wordfrequency():
+    text_cleaned2, text_cleaned0  = text_clean_run()
+    print(collections.Counter(text_cleaned0))
 
 
 if __name__ == "__main__":
-    lda_model()
+    # lda_model()
+    # tfidf_model()
+    # kmeans_model()
+    get_wordfrequency()
+
+
+    # 接下来就是 聚类方法试一下；写出lda的循环调参并绘图
