4a3ee48b6e649e99e4f5235777d0d90cd971cf8c
CCNUHemingZhang <CCNUHemingZhang@gmail.com>
2019-08-04 21:01:28 +0800

    finshed

--- test.py
+++
@@ -1,8 +0,0 @@
-
-A = 'hello, world!'
-for i in range(1):
-    A = A + ' ' + A
-    print(A)
-
-
-
--- testcrawl0.py
+++
@@ -1,9 +0,0 @@
-#coding:utf-8
-import urllib.request
-
-def run_demo():
-    crawl = urllib.request.urlopen('http://www.baidu.com')
-    print(crawl.read())
-
-if __name__=='__main__':
-    run_demo()
